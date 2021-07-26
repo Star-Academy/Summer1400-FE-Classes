8c38946da9b29b37f2eab99c84ba2b4ce0d08593
BijanEisapour <bijanprogrammer@gmail.com>
2021-07-26 09:50:03 +0430

    [ADD] slideshow

---
+++ scripts/slideshow.js
@@ -0,0 +1,13 @@
+// CONFIG
+const COLORS = [
+    'ff441f',
+    'ffb41f',
+    '8fff1f',
+    '1e90ff',
+    'ff1f7c',
+];
+
+// sample url: https://via.placeholder.com/1920x1080/1a1a1a/fafafa?text=some_text
+
+// MAIN
+// your code here ...
