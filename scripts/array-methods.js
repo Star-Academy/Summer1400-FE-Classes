f341f67dcf23fdb46994598a0c3ae46b652bd505
BijanEisapour <bijanprogrammer@gmail.com>
2021-07-26 09:44:36 +0430

    [ADD] array methods

--- scripts/array-methods.js
+++ scripts/array-methods.js
@@ -1,5 +1,39 @@
+// users
+logSeparator('users');
+
+const users = [
+    {
+        firstName: 'Bijan',
+        lastName: 'Eisapour',
+        major: 'CE',
+        job: 'Frontend Developer',
+    },
+    {
+        firstName: 'Reza',
+        lastName: 'Eisapour',
+        major: 'CE',
+        job: 'Software Engineer',
+    },
+    {
+        firstName: 'Javad',
+        lastName: 'Ezati',
+        major: 'N/A',
+        job: 'Actor',
+    },
+    {
+        firstName: 'Shahab',
+        lastName: 'Hosseini',
+        major: 'N/A',
+        job: 'Actor',
+    },
+];
+
+// TODO: challenge - is there a better way?
+console.log(users);
+
 // numbers
 logSeparator('numbers');
+
 const numbers = [4, 8, 15, 16, 23, 42];
 console.log(numbers);
 
